e1f03b1c30cbb56ef387483967a71c326c1d3e81
Amantha Jayathilake <amanthajayathilake@gmail.com>
2021-12-16 09:55:31 +0530

    m

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useRef } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Home from "./components/Home";
+import NavBar from "./components/navbar";
+import Register from "./components/Register";
+import YourTicket from "./components/YourTicket";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <Router>
+          <NavBar />
+          <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/Register" exact component={Register} />
+          <Route path="/Ticket" exact component={YourTicket} />
+          <Route path="/About" exact component={About} />
+          <Route path="/Contact" exact component={Contact} />
+        </Switch>
+      </Router>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "../App.css";
+import { Button } from "@material-ui/core";
+
+export default function Home() {
+  return (
+    <div className="main">
+      <center>
+        <div class="container">
+          <h1 className="mainHeading">HOLA MOZILLA 2.0</h1>
+          <div className="dateText">
+            <span>21</span>
+            <span>st</span> <span>December @ 7.00pm</span>
+          </div>
+
+          <div id="countdown" className="countdown glowing">
+            <ul>
+              <li>
+                <span id="days"></span>
+                <tx style={{ color: "#f83600" }}>days</tx>
+              </li>
+              <li>
+                <span id="hour"></span>
+                <tx style={{ color: "#f83600" }}>Hours</tx>
+              </li>
+              <li>
+                <span id="minute"></span>
+                <tx style={{ color: "#f83600" }}>Minutes</tx>
+              </li>
+              <li>
+                <span id="second"></span>
+                <tx style={{ color: "#f83600" }}>Seconds</tx>
+              </li>
+              {/* <li><span id="end"></span><tx style={{color: "#f83600"}}>Seconds</tx></li> */}
+            </ul>
+          </div>
+
+          <img
+            className="watermark"
+            src="https://res.cloudinary.com/fitness-glory/image/upload/v1639484985/logo_icon-01_s6oex8.png"
+          />
+
+          <Button className="myBtn">
+            <a style={{ textDecoration: "none" }}>Register Now</a>
+          </Button>
+        </div>
+      </center>
+    </div>
+  );
+}
